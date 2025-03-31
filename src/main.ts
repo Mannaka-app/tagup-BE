463b73919d69686687c3ecda20ff2a976a9502ca
Dong Hyun <decidere@naver.com>
2025-04-01 04:16:15 +0900

    [Feat] DTO 사용을 위한 글로벌 파이프 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에 정의되지 않은 필드 제거
+      forbidNonWhitelisted: true, // 정의되지 않은 필드가 있으면 에러
+      transform: true, // class-transformer 적용
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
