1ab9cc02831b02d87db2a8ee6341d6c2bb52a53b
Dong Hyun <decidere@naver.com>
2025-04-01 04:26:12 +0900

    [Feat] axios 테스트용 로직 구현

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async register(registerDto: RegisterDto) {
+    await this.prisma.test.create({
+      data: {
+        name: registerDto.name,
+        email: registerDto.email,
+        password: registerDto.password,
+      },
+    });
+
+    return { success: true, message: '회원가입이 완료되었습니다.' };
+  }
+}
