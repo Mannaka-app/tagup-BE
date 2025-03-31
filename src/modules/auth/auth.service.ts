4a3795f87116c0220a40c2d8f7f89f80ee312ceb
Dong Hyun <decidere@naver.com>
2025-04-01 06:15:03 +0900

    [Refactor] 회원가입 로직 고도화

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RegisterDto } from './dto/register.dto';
 
@@ -7,14 +11,27 @@ export class AuthService {
   constructor(private readonly prisma: PrismaService) {}
 
   async register(registerDto: RegisterDto) {
-    await this.prisma.test.create({
-      data: {
-        name: registerDto.name,
-        email: registerDto.email,
-        password: registerDto.password,
-      },
+    const exist = await this.prisma.users.findUnique({
+      where: { email: registerDto.email },
     });
 
-    return { success: true, message: '회원가입이 완료되었습니다.' };
+    if (exist) {
+      throw new ConflictException('이미 등록된 이메일입니다.');
+    }
+    try {
+      await this.prisma.users.create({
+        data: {
+          name: registerDto.name,
+          email: registerDto.email,
+          password: registerDto.password,
+          authProvider: 'Tagup',
+        },
+      });
+
+      return { success: true, message: '회원가입이 완료되었습니다.' };
+    } catch (error) {
+      console.error('회원가입 중 오류 발생:', error);
+      throw new InternalServerErrorException('서버에서 오류가 발생했습니다.');
+    }
   }
 }
