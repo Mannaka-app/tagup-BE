f737922fddf06bcc43897ec7c97b67ffdf814e9a
Dong Hyun <decidere@naver.com>
2025-04-02 06:36:17 +0900

    [Feat] 유저 추가 정보 설정 로직 구현

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { UserDetailDto } from './dto/userDetail.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post('detail')
+  async setUserDetail(@Req() req, @Body() userDetailDto: UserDetailDto) {
+    return await this.usersService.setUserDetail(req.user, userDetailDto);
+  }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Gender } from '@prisma/client';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async setUserDetail(user, data) {
+    const { userId } = user;
+
+    const exist = await this.prisma.users.findUnique({
+      where: { id: userId },
+    });
+
+    if (!exist) {
+      throw new UnauthorizedException('등록되지 않은 유저입니다.');
+    }
+
+    try {
+      const gender: Gender =
+        data.gender == 'MALE' ? Gender.MALE : Gender.FEMALE;
+
+      await this.prisma.users.update({
+        where: { id: userId },
+        data: {
+          nickname: data.nickname,
+          gender: gender,
+        },
+      });
+
+      return { success: true, message: '성별 및 닉네임 설정이 완료됐습니다.' };
+    } catch (err) {
+      console.error(err);
+      throw new InternalServerErrorException('서버에서 오류가 발생했습니다.');
+    }
+  }
+}
