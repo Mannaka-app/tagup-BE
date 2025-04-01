5b8415c398385bb6dac5f968a3facf541ff1584e
Dong Hyun <decidere@naver.com>
2025-04-01 18:42:40 +0900

    [Feat] Redis 서비스 로직 및 모듈 구현

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import Redis from 'ioredis';
 
 @Injectable()
-export class RedisService {}
+export class RedisService implements OnModuleInit {
+  private client: Redis;
+
+  onModuleInit() {
+    this.client = new Redis({
+      host: '127.0.0.1',
+      port: 6379,
+    });
+  }
+
+  async set(key: string, value: string, ttlSeconds: number) {
+    await this.client.set(key, value, 'EX', ttlSeconds);
+  }
+
+  async get(key: string): Promise<string | null> {
+    return await this.client.get(key);
+  }
+
+  async del(key: string) {
+    return await this.client.del(key);
+  }
+}
