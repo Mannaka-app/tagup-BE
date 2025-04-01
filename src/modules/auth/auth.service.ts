2cd8a68aa7f99b5498fe95e6cccf10cec2376b2e
Dong Hyun <decidere@naver.com>
2025-04-01 18:37:23 +0900

    [Feat] 로그인 로직 구현

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -2,14 +2,20 @@ import {
   ConflictException,
   Injectable,
   InternalServerErrorException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RegisterDto } from './dto/register.dto';
 import * as bcrypt from 'bcrypt';
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwt: JwtService,
+  ) {}
 
   async register(registerDto: RegisterDto) {
     const exist = await this.prisma.users.findUnique({
@@ -35,6 +41,33 @@ export class AuthService {
       return { success: true, message: '회원가입이 완료되었습니다.' };
     } catch (error) {
       console.error('회원가입 중 오류 발생:', error);
+      throw new InternalServerErrorException('서버에서 오류가 발생했습니다.');
+    }
+  }
+
+  async login(loginDto: LoginDto) {
+    try {
+      const user = await this.prisma.users.findUnique({
+        where: { email: loginDto.email },
+      });
+
+      if (!user) throw new UnauthorizedException('계정이 존재하지 않습니다.');
+
+      const validPassword = await bcrypt.compare(
+        loginDto.password,
+        user.password,
+      );
+
+      if (validPassword) {
+        const accessToken = this.jwt.sign({
+          userId: user.id,
+        });
+        return { success: true, message: '로그인에 성공했습니다', accessToken };
+      } else {
+        throw new UnauthorizedException('비밀번호가 일치하지 않습니다.');
+      }
+    } catch (err) {
+      if (err instanceof UnauthorizedException) throw err;
       throw new InternalServerErrorException('서버에서 오류가 발생했습니다.');
     }
   }
