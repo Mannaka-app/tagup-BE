8f27ba5f8196b1a419d90ef08927a8cbabbbdcd4
Dong Hyun <decidere@naver.com>
2025-04-03 20:34:49 +0900

    [Feat] 경기 일정 조회 로직 구현

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -4,4 +4,9 @@ import { GameService } from './game.service';
 @Controller('game')
 export class GameController {
   constructor(private readonly gameService: GameService) {}
+
+  @Get('schedule')
+  async getGameSchedules() {
+    return await this.gameService.getGameSchedules();
+  }
 }
--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -4,4 +4,33 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class GameService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async getGameSchedules() {
+    const result = await this.prisma.gameSchedule.findMany({
+      orderBy: { date: 'asc' },
+
+      include: {
+        homeTeam: true,
+        awayTeam: true,
+        stadiumInfo: true,
+      },
+    });
+
+    const schedules = result.map((res) => ({
+      id: res.id,
+      home: res.homeTeam,
+      away: res.awayTeam,
+      stadium: res.stadiumInfo,
+      date: res.date,
+      status: res.status,
+      score: res.score,
+      winner: res.win
+        ? res.win == res.homeTeam.id
+          ? res.homeTeam
+          : res.awayTeam
+        : 0,
+    }));
+
+    return schedules;
+  }
 }
