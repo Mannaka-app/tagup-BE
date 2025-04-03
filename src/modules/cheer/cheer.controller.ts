e2dfe628c120762a66afcdfb1dd16847f6c29e41
Dong Hyun <decidere@naver.com>
2025-04-04 03:33:09 +0900

    [Feat] 응원 작성하기 로직 구현

--- src/modules/cheer/cheer.controller.ts
+++ src/modules/cheer/cheer.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { CheerService } from './cheer.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('cheer')
 export class CheerController {
   constructor(private readonly cheerService: CheerService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async createCheerTalk(@Req() req, @Body() data) {
+    return await this.cheerService.createCheerTalk(req.user, data.content);
+  }
 }
--- src/modules/cheer/cheer.service.ts
+++ src/modules/cheer/cheer.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
-export class CheerService {}
+export class CheerService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly usersService: UsersService,
+  ) {}
+
+  async createCheerTalk(user, content: string) {
+    const { userId } = user;
+
+    const data = await this.usersService.getUserById(userId);
+
+    await this.prisma.cheerTalk.create({
+      data: {
+        userId,
+        content,
+        createdAt: new Date(),
+        team: data.team,
+      },
+    });
+
+    return { success: true, message: '응원 등록이 완료됐습니다.' };
+  }
+}
