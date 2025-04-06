6ef61986bfcd591c690a357b79e6274da7ed0825
Dong Hyun <decidere@naver.com>
2025-04-06 22:46:00 +0900

    [Feat] 피드 이미지 업로드 로직 구현

--- src/feeds/feeds.controller.ts
+++ src/feeds/feeds.controller.ts
@@ -1,5 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FeedsService } from './feeds.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('feeds')
 export class FeedsController {
@@ -8,5 +15,11 @@ export class FeedsController {
   @Get('tags')
   async getAllTags() {
     return await this.feedsService.getAllTags();
+  }
+
+  @Post('image')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadImage(@UploadedFile() file: Express.Multer.File) {
+    return await this.feedsService.uploadFeedImage(file);
   }
 }
--- src/feeds/feeds.service.ts
+++ src/feeds/feeds.service.ts
@@ -1,9 +1,17 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { S3Service } from 'src/s3/s3.service';
 
 @Injectable()
 export class FeedsService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly s3: S3Service,
+  ) {}
 
   async getAllTags() {
     try {
@@ -13,6 +21,27 @@ export class FeedsService {
     } catch (error) {
       console.error('태그 목록 불러오는 중 에러 발생:', error);
       throw new InternalServerErrorException('서버에서 오류가 발생했습니다.');
+    }
+  }
+
+  async uploadFeedImage(file: Express.Multer.File) {
+    try {
+      const fileType = file.mimetype.split('/')[1];
+      if (!fileType) {
+        throw new BadRequestException('유효하지 않은 파일 형식입니다.');
+      }
+
+      const imageUrl = await this.s3.uploadImage(
+        8,
+        file.buffer,
+        fileType,
+        'feed/',
+      );
+
+      return { success: true, imageUrl };
+    } catch (error) {
+      console.error('이미지 업로드 중 오류 발생', error);
+      throw new InternalServerErrorException('이미지 업로드에 실패했습니다.');
     }
   }
 }
