06f52771bd2266cbc03b013abc164b504bf2c5df
Dong Hyun <decidere@naver.com>
2025-04-06 22:11:32 +0900

    [Feat] 피드 태그 데이터 조회 로직 구현

--- src/feeds/feeds.controller.ts
+++ src/feeds/feeds.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { FeedsService } from './feeds.service';
 
 @Controller('feeds')
 export class FeedsController {
   constructor(private readonly feedsService: FeedsService) {}
+
+  @Get('tags')
+  async getAllTags() {
+    return await this.feedsService.getAllTags();
+  }
 }
--- src/feeds/feeds.service.ts
+++ src/feeds/feeds.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class FeedsService {}
+export class FeedsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getAllTags() {
+    try {
+      const tags = await this.prisma.tags.findMany();
+
+      return { success: true, tags };
+    } catch (error) {
+      console.error('태그 목록 불러오는 중 에러 발생:', error);
+      throw new InternalServerErrorException('서버에서 오류가 발생했습니다.');
+    }
+  }
+}
