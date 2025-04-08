c474d7309e4e40a257b471025c33a6153f93455d
Dong Hyun <decidere@naver.com>
2025-04-08 23:39:15 +0900

    [Feat] 연결 테스트용 게이트웨이 구현

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,7 +1,18 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { ChatService } from './chat.service';
+import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({ namespace: '/chat', cors: { origin: '*' } })
 export class ChatGateway {
   constructor(private readonly chatService: ChatService) {}
+
+  @WebSocketServer() server: Server;
+
+  hadleConnection(client: Socket) {
+    console.log('유저 연결됨 :', client.id);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log('❌ 연결 해제됨:', client.id);
+  }
 }
