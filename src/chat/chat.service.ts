cad6fc3818a1e74316bd6c8e44941cc6da31f8db
Dong Hyun <decidere@naver.com>
2025-04-09 20:21:56 +0900

    [Feat] 테스트용 채팅방 목록 조회 로직 구현

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getRooms() {
+    return await this.prisma.rooms.findMany();
+  }
+}
