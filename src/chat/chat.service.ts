dc0a5b79058e4401315a3181f515daf8a613f0c1
Dong Hyun <decidere@naver.com>
2025-04-12 20:27:58 +0900

    [Refactor] 전체 채팅방 조회 로직 응답데이터 수정

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -10,12 +10,18 @@ export class ChatService {
       include: { RoomUsers: true },
     });
 
-    return result.map((res) => ({
+    const rooms = result.map((res) => ({
       id: res.id,
       title: res.title,
       createAt: res.createdAt,
       members: res.RoomUsers.length,
     }));
+
+    return {
+      success: true,
+      message: '전체 채팅방 조회에 성공했습니다.',
+      rooms,
+    };
   }
 
   async getMessages(userId: number, roomId: number) {
